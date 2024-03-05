d341310cc399492d04c82ecb747890722349dcf2
podestal <luisrodriguezpodesta@gmail.com>
2024-03-05 10:19:36 -0500

    setup reminders

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReminderList from './components/ReminderList'
+import Reminder from './models/Reminder'
+
+const reminders: Reminder[] = [
+  {id: 1, title: 'Reminder 1'},
+  {id: 2, title: 'Reminder 2'},
+  {id: 3, title: 'Reminder 3'}
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Reminders TypeScript - React</h1>
+      <ReminderList 
+        reminders={reminders}
+      />
     </>
   )
 }
---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,16 @@
+
+import Reminder from "../models/Reminder"
+
+interface ReminderListProps {
+    reminders: Reminder[]
+}
+
+const ReminderList = ({reminders}: ReminderListProps) => {
+  return (
+    <ul>
+        {reminders.map(reminder => <li key={reminder.id}>{reminder.title}</li>)}
+    </ul>
+  )
+}
+
+export default ReminderList
